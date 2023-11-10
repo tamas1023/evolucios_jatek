2469b504a427fce61ca16c838487c909d8c84cb9
tamas1023 <bodnar.tamas.1023@gmail.com>
2023-11-10 18:02:45 +0100

    Pálya bevezetése

---
+++ Palya/palya.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Palya
+{
+    int palyameret;
+    vector<vector<int *>> palya;
+
+public:
+    Palya(const int &palyameret) : palyameret(palyameret)
+    {
+        for (int i = 0; i < palyameret; i++)
+        {
+            palya.push_back(vector<int *>()); //
+            for (int j = 0; j < palyameret; j++)
+            {
+                palya[i].push_back(nullptr);
+            }
+        }
+    };
+    void palyaKiiratasa()
+    {
+        // első oszlop
+        std::cout << "     ";
+        for (int i = 0; i < palyameret; i++)
+        {
+            if (i < 10)
+            {
+                std::cout << i << "   ";
+            }
+            else
+            {
+                std::cout << i << "  ";
+            }
+        }
+        std::cout << std::endl;
+        for (int i = 0; i < palyameret; i++)
+        {
+            cout << "---|";
+        }
+        cout << "---|" << endl;
+        // sorok
+        for (int i = 0; i < palyameret; i++)
+        {
+            if (i < 10)
+            {
+                std::cout << " " << i << " | ";
+            }
+            else
+            {
+                std::cout << i << " | ";
+            }
+            for (int j = 0; j < palyameret; j++)
+            {
+                // TODO átállítani majd == re mert ez ideiglenes
+                if (palya[i][j] == nullptr)
+                {
+                    std::cout << 0 << " | ";
+                }
+                else
+                {
+                    // int szam = rand() % (palyameret * palyameret - 2 + 1) + 2;
+                    // TODO majd itt nem lesz elég csak a palya i j edik elemét kiíratni
+                    int szam = *palya[i][j];
+                    if (szam >= 10)
+                    {
+                        std::cout << szam << "| ";
+                    }
+                    else
+                    {
+                        std::cout << szam << " | ";
+                    }
+                }
+            }
+            cout << endl;
+            for (int i = 0; i < palyameret; i++)
+            {
+                cout << "---|";
+            }
+            cout << "---|" << endl;
+        }
+    }
+};
--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <vector>
 
+#include "Eloleny/eloleny.cpp"
+#include "Palya/palya.cpp"
+
 using namespace std;
 
 int palyameret;
@@ -13,68 +16,7 @@ vector<vector<int *>> palya;
 
 // ezt majd át kell rakni a playa.cpp be, meg majd a többit is
 // meg majd az int * helyett majd Eloleny típusút kell megcsinálni
-void palyaKiiratasa()
-{
-    // első oszlop
-    std::cout << "     ";
-    for (int i = 0; i < palyameret; i++)
-    {
-        if (i < 10)
-        {
-            std::cout << i << "   ";
-        }
-        else
-        {
-            std::cout << i << "  ";
-        }
-    }
-    std::cout << std::endl;
-    for (int i = 0; i < palyameret; i++)
-    {
-        cout << "---|";
-    }
-    cout << "---|" << endl;
-    // sorok
-    for (int i = 0; i < palyameret; i++)
-    {
-        if (i < 10)
-        {
-            std::cout << " " << i << " | ";
-        }
-        else
-        {
-            std::cout << i << " | ";
-        }
-        for (int j = 0; j < palyameret; j++)
-        {
-            // TODO átállítani majd == re mert ez ideiglenes
-            if (palya[i][j] == nullptr)
-            {
-                std::cout << 0 << " | ";
-            }
-            else
-            {
-                // int szam = rand() % (palyameret * palyameret - 2 + 1) + 2;
-                // TODO majd itt nem lesz elég csak a palya i j edik elemét kiíratni
-                int szam = *palya[i][j];
-                if (szam >= 10)
-                {
-                    std::cout << szam << "| ";
-                }
-                else
-                {
-                    std::cout << szam << " | ";
-                }
-            }
-        }
-        cout << endl;
-        for (int i = 0; i < palyameret; i++)
-        {
-            cout << "---|";
-        }
-        cout << "---|" << endl;
-    }
-}
+
 int main(int argc, char const *argv[])
 {
 
@@ -126,15 +68,13 @@ int main(int argc, char const *argv[])
         try
         {
             string valasz;
-            cout << "Adja meg a jelenlegi elolenyek szamat (2-tol, maximum(palyameret ig), ha nem ad meg szamot, akkor veletlen darab lesz hozzaadva: " << endl;
+            cout << "Adja meg a jelenlegi elolenyek szamat (2-tol, maximum palyameret * palyameret-ig, ha nem ad meg szamot, akkor veletlen darab lesz hozzaadva: " << endl;
             getline(cin, valasz);
-            if (valasz.length() > 0)
+
+            elolenydb = stoi(valasz);
+            if (elolenydb >= 2 && elolenydb <= palyameret * palyameret)
             {
-                elolenydb = stoi(valasz);
-                if (elolenydb >= 2 && elolenydb <= palyameret)
-                {
-                    joeloleny = true;
-                }
+                joeloleny = true;
             }
             else
             {
@@ -163,15 +103,9 @@ int main(int argc, char const *argv[])
         }
     }
 
+    Palya palya(palyameret);
+    palya.palyaKiiratasa();
     // vector<vector<int *>> palya(palyameret, vector<int *>(palyameret));
-    for (int i = 0; i < palyameret; i++)
-    {
-        palya.push_back(vector<int *>()); //
-        for (int j = 0; j < palyameret; j++)
-        {
-            palya[i].push_back(nullptr);
-        }
-    }
-    palyaKiiratasa();
+
     return 0;
 }
